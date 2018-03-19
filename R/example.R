75aea5ede96b9082dcf4f03fe930b71dd16e32c5
petulla <sam.petulla@gmail.com>
2018-03-19 19:23:23 -0400

    Update main function and example datasets.

---
+++ R/example.R
@@ -0,0 +1,17 @@
+#' Get path to readroper example
+#'
+#' readroper comes bundled with a number of sample files in its `inst/extdata`
+#' directory. This function make them easy to access
+#'
+#' @param path Name of file. If `NULL`, the example files will be listed.
+#' @export
+#' @examples
+#' readroper_example()
+#' readroper_example("processData.R")
+readroper_example <- function(path = NULL) {
+  if (is.null(path)) {
+    dir(system.file("extdata", package = "readroper"))
+  } else {
+    system.file("extdata", path, package = "readroper", mustWork = TRUE)
+  }
+}
--- R/readroper.R
+++ R/readroper.R
@@ -12,27 +12,48 @@
 #' @param widths
 #' @param col_names
 #' @param filepath
+#' @param card_read
+#' @param cards
 #' @return a dataframe with \code{len(col_names)} number of columns, assigned to the values of \code{col_names}
+#' @export
+#' @examples
+#' fwf_sample <- readr_example("testMultiCard.txt")
+#' cat(read_lines(fwf_sample))
+#' fwf_sample2 <- readr_example("testSingleCard.txt")
+#' cat(read_lines(fwf_sample2))
+#' # 1. Fixed width file, first card, multi-card
+#' read_rpr(c(1,2,4), c(1,2,1), c('V1','V2','V3'), fwf_sample, 1, 2)
+#' # 2 .Fixed width file, first card, single card
+#' read_rpr(c(1,2,4), c(1,2,1), c('V1','V2','V3'), fwf_sample2)
+#' # 3. Fixed width file, second card, multi-card
+#' read_rpr(c(2,1,4), c(2,1,1), c('V2','V1','V3'), fwf_sample, 2, 2)
 #'
-#' @examples read_rpr(c('1','2','4'), c('1','2','1'), c('V1','V2','V3'), './', 1)
-#' @examples read_rpr(c('1','2','4'), c('1','2','1'), c('V1','V2','V3'), './')
-#' @examples read_rpr(c('2','1','4'), c('2','1','1'), c('V2','V1','V3'), './', 2)
-
-
-#' @export read_rpr
 read_rpr <- function(col_positions, widths, col_names, filepath, card_read, cards) {
-    
+
+    if (is.nan(filepath)) {
+      print('File must be provided as path or object')
+    }
+
+    if (is.nan(col_positions) || is.nan(widths) || is.nan(col_names)) {
+      print("Col positions, widths and names must be given")
+    }
     if (length(unique(list(col_names, col_positions, widths))) == 1) {
         print("The lengths of the vectors of column names, widths, and positions must be the same")
         break
     }
+
+    if ((is.nan(cards) && !is.nan(card_read)) || (!is.nan(cards) && is.nan(card_read))) {
+        print("If reading a multi-card dataset, the number of cards and the card number to read from must be provided.")
+        break
+    }
+
     end_positions <- col_positions + widths - 1
-    
-    if (is.NaN(cards)) {
-        return(read_fwf(filepath, fwf_positions(col_positions, end_positions, col_names)))
+
+    if (is.nan(cards) || is.nan(card_read)) {
+        return(readr::read_fwf(filepath, readr::fwf_positions(col_positions, end_positions, col_names)))
     } else {
         card_v <- rep(FALSE, cards)
-        card_v(c(card_read)) <- FALSE
-        return(read_fwf(filepath, fwf_positions(col_positions, end_positions, col_names)))[card_v, ]
+        card_v[card_read] <- TRUE
+        return(readr::read_fwf(filepath, readr::fwf_positions(col_positions, end_positions, col_names)))[card_v, ]
     }
 }
