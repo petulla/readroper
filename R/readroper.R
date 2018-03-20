6a7ca9d2373f34401eedb56022aa4aafc799eab3
petulla <sam.petulla@gmail.com>
2018-03-19 23:57:29 -0400

    Add readme and vignette start

--- R/readroper.R
+++ R/readroper.R
@@ -3,6 +3,7 @@
 #' @description Reads fixed-width ASCII roper files.
 
 #' Takes arguments in the format a Roper Center survey codebook provides
+#'
 #' @param col_positions starting position(s) to read
 #' @param widths width of value(s) to read
 #' @param col_names names of columns to return
@@ -27,32 +28,32 @@
 #' col_names=c('V1','V2','V3'), filepath=fwf_sample, card_read=2, cards=2)
 #'
 read_rpr <- function(col_positions = NaN, widths = NaN, col_names = NaN, filepath = NaN, card_read = NaN, cards = NaN) {
-
+    
     if (is.nan(filepath)) {
         stop("File must be provided as path or object", call. = FALSE)
     } else if (is.nan(col_positions) || is.nan(widths) || is.nan(col_names)) {
         stop("Col positions, widths and names must be given", call. = FALSE)
     } else if (length(unique(sapply(list(widths, col_positions, col_names), length))) > 1) {
         stop("The lengths of the vectors of column names, widths, and positions must be the same", call. = FALSE)
-    } else if ((is.nan(cards) & !is.nan(card_read)) || (!is.nan(cards) & is.nan(card_read) & as.numeric(card_read) !=
+    } else if ((is.nan(cards) & !is.nan(card_read)) || (!is.nan(cards) & is.nan(card_read) & as.numeric(card_read) != 
         1)) {
-        stop("If reading a multi-card dataset, the number of cards and the card number to read from must be provided.",
+        stop("If reading a multi-card dataset, the number of cards and the card number to read from must be provided.", 
             call. = FALSE)
     }
-
+    
     widths <- as.numeric(widths)
     col_positions <- as.numeric(col_positions)
-
+    
     end_positions <- col_positions + widths - 1
-
+    
     if (!is.nan(cards)) {
         cards = as.numeric(cards)
     }
-
+    
     if (!is.nan(card_read)) {
         card_read <- as.numeric(card_read)
     }
-
+    
     if ((cards == 1) || (is.nan(cards) & is.nan(card_read))) {
         return(readr::read_fwf(filepath, readr::fwf_positions(col_positions, end_positions, col_names)))
     } else {
---
+++ README.md
@@ -0,0 +1,1 @@
+See the [vignettes](https://github.com/petulla/readroper/tree/master/vignettes) for how to use the package.
