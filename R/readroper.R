78391f07ddd4f96be4d32008fc602111222b63d5
Frederick Solt <fsolt@mac.com>
2018-06-25 07:20:53 -0500

    make read_rpr an alias to read_ascii but with original read_rpr arguments (for backwards compatibility)

--- R/readroper.R
+++ R/readroper.R
@@ -8,9 +8,9 @@
 #' @param widths width of value(s) to read
 #' @param col_names names of columns to return
 #' @param filepath data obj or filepath to read
-#' @param card_read card to read (if blank, reads a single card dataset)
+#' @param card_read a vector of card(s) to read (if blank, reads a single card dataset)
 #' @param cards number of cards in dataset (if null, assumes single card)
-#' @return a dataframe with \code{len(col_names)} number of columns, assigned to the values of \code{col_names}
+#' @return A data frame containing the variables specified in the \code{col_names} argument, plus a numeric \code{respondent} identifier and as many string \code{card} variables (\code{card1}, \code{card2}, ...) as specified by the \code{cards} argument.
 #' @export
 #' @examples
 #' fwf_sample <- readroper_example('testMultiCard.txt')
@@ -27,38 +27,24 @@
 #' read_rpr(col_positions=c(1,2,4), widths=c(1,2,1),
 #' col_names=c('V1','V2','V3'), filepath=fwf_sample, card_read=2, cards=2)
 #'
-read_rpr <- function(col_positions = NaN, widths = NaN, col_names = NaN, filepath = NaN, card_read = NaN, cards = NaN) {
-    
-    if (is.nan(filepath)) {
-        stop("File must be provided as path or object", call. = FALSE)
-    } else if (is.nan(col_positions) || is.nan(widths) || is.nan(col_names)) {
-        stop("Col positions, widths and names must be given", call. = FALSE)
-    } else if (length(unique(sapply(list(widths, col_positions, col_names), length))) > 1) {
-        stop("The lengths of the vectors of column names, widths, and positions must be the same", call. = FALSE)
-    } else if ((is.nan(cards) & !is.nan(card_read)) || (!is.nan(cards) & is.nan(card_read) & as.numeric(card_read) != 
-        1)) {
-        stop("If reading a multi-card dataset, the number of cards and the card number to read from must be provided.", 
-            call. = FALSE)
-    }
-    
-    widths <- as.numeric(widths)
-    col_positions <- as.numeric(col_positions)
-    
-    end_positions <- col_positions + widths - 1
-    
-    if (!is.nan(cards)) {
-        cards = as.numeric(cards)
-    }
-    
-    if (!is.nan(card_read)) {
-        card_read <- as.numeric(card_read)
-    }
-    
-    if ((cards == 1) || (is.nan(cards) & is.nan(card_read))) {
-        return(readr::read_fwf(filepath, readr::fwf_positions(col_positions, end_positions, col_names)))
-    } else {
-        card_v <- rep(FALSE, cards)
-        card_v[card_read] <- TRUE
-        return(readr::read_fwf(filepath, readr::fwf_positions(col_positions, end_positions, col_names)))[card_v, ]
-    }
+read_rpr <- function(col_positions = NaN, widths = NaN, col_names = NaN, filepath = NaN, card_read = 1, cards = 1) {
+
+  if (is.nan(filepath)) {
+    stop("File must be provided as path or object", call. = FALSE)
+  } else if (is.nan(col_positions) || is.nan(widths) || is.nan(col_names)) {
+    stop("Col positions, widths and names must be given", call. = FALSE)
+  } else if (length(unique(sapply(list(widths, col_positions, col_names), length))) > 1) {
+    stop("The lengths of the vectors of column names, widths, and positions must be the same", call. = FALSE)
+  } else if ((max(card_read) > max(cards))) {
+    stop("If reading a multi-card dataset, the numbers of the cards to be read must not exceed the total number of cards.",
+         call. = FALSE)
+  }
+
+  x <- read_ascii(file = filepath,
+                  total_cards = cards,
+                  var_names = col_names,
+                  var_cards = card_read,
+                  var_positions = col_positions,
+                  var_widths = widths)
+  return(x)
 }
