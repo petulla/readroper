6c6c9bfe92db4d3847c574f1231dbdae8721bf93
petulla <sam.petulla@gmail.com>
2018-06-25 01:17:31 -0400

    Update test

--- tests/testthat/test-readroper.R
+++ tests/testthat/test-readroper.R
@@ -4,7 +4,7 @@ test_that("First row of multicard dataset matches", {
   out1 <- read_rpr(col_positions=c(1,2,4), widths=c(1,2,1),
                    col_names=c('V1','V2','V3'), filepath='testMultiCard.txt', card_read=2, cards=2)
   expect_equal(out1$V1[1], 0)
-  expect_equal(out1$V2[1], "08")
+  expect_equal(out1$V2[1], "80")
   expect_equal(out1$V3[1], 1)
 })
 
